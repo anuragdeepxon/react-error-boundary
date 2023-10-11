c87b737feff69bbe68eadf89889e29b7898efabc
Anurag Deep <anuragdeep.xon@gmail.com>
2023-10-11 22:54:08 +0530

    registery error

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+"use client";
+export * from "./ErrorBoundary";
+export * from "./ErrorBoundaryContext";
+export * from "./useErrorBoundary";
+export * from "./withErrorBoundary";
+
+// TypeScript types
+export * from "./types";
